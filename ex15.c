0c2b9c9969b040babe5a9b7dec7e3557567fe16e
Your Name <you@example.com>
2025-03-19 22:50:12 +0200

    ex15 start

---
+++ ex15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// main function
+int main(int argc, char *argv[])
+{
+
+    int ages[] = {23, 43, 12, 89, 2};
+
+    char *names[] = {"Alan", "Frank",
+                     "Mary", "John", "Lisa"};
+
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
+
+    return 0;
+}
