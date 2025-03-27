2b5b89c2b65184fe1e2743a11fb8aa1033cc56d5
Your Name <you@example.com>
2025-03-28 00:34:54 +0200

    ex15 pointers complete writting

--- ex15.c
+++ ex15.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 
 // main function
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    int ages[] = {23, 43, 12, 89, 2};
+  int ages[] = {23, 43, 12, 89, 2};
 
-    char *names[] = {"Alan", "Frank",
-                     "Mary", "John", "Lisa"};
+  char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa"};
 
-    int count = sizeof(ages) / sizeof(int);
-    int i = 0;
+  int count = sizeof(ages) / sizeof(int);
+  int i = 0;
 
-    return 0;
+  // first way using indexing
+  for (i = 0; i < count; i++) {
+    printf("%s has %d years alive", names[i], ages[i]);
+  }
+
+  printf("-----\n");
+
+  // set up the pointers to the start of the array
+  int *cur_age = ages;
+  char **cur_name = names;
+
+  // second way using pointers
+  for (i = 0; i < count; i++) {
+    printf("%s is %d years old.\n", *(cur_name + i), *(cur_age + i));
+  }
+
+  printf("----/n");
+
+  // third way using pointers
+
+  for (i = 0; i < count; i++) {
+    printf("%s is %d years old . \n", cur_name[i], cur_age[i]);
+  }
+
+  printf("----/n");
+
+  // fourth way to use pointers (stupid complex way)
+  for (cur_name = names, cur_age = ages; (cur_name - ages), count;
+       cur_name++, cur_age++) {
+
+    printf("%s lived for %d years. \n", *cur_name, *cur_age);
+  }
+
+  return 0;
 }
